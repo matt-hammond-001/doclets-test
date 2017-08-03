1e4ed79917216607d2002080b7ee1af475bba182
Matt Hammond <matt-hammond-bbc@users.noreply.github.com>
2017-08-03 13:12:56 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Test function
+ * @param {String} parameter Description of parameter
+ * @param {Number} [optional] Optional value
+ */
+function foo(parameter, optional) {
+  console.log(parameter)
+  console.log(optional)
+}
+
